75f081491ee13e1df074fb5439b85538e82b922d
daniels75 <daniel.sadowski75@gmail.com>
2019-11-06 00:26:24 +0100

    Added SimpleController

--- di-demo/src/main/java/org/daniels/spring/didemo/DiDemoApplication.java
+++ di-demo/src/main/java/org/daniels/spring/didemo/DiDemoApplication.java
@@ -1,13 +1,17 @@
 package org.daniels.spring.didemo;
 
+import org.daniels.spring.didemo.controllers.SimpleController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DiDemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DiDemoApplication.class, args);
+        final ApplicationContext appContext = SpringApplication.run(DiDemoApplication.class, args);
+        SimpleController simpleController  = (SimpleController)appContext.getBean("simpleController");
+        simpleController.hello();
     }
 
 }
---
+++ di-demo/src/main/java/org/daniels/spring/didemo/controllers/SimpleController.java
@@ -0,0 +1,12 @@
+package org.daniels.spring.didemo.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class SimpleController {
+
+    public String hello() {
+        System.out.println("Hello");
+        return "Hello";
+    }
+}
