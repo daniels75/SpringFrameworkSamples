3648356075df8492a010d44d8123125abd4e200a
daniels75 <daniel.sadowski75@gmail.com>
2019-11-07 20:29:15 +0100

    Added Qualified bean injection example

--- di-demo/src/main/java/org/daniels/spring/didemo/controllers/SetterInjectedController.java
+++ di-demo/src/main/java/org/daniels/spring/didemo/controllers/SetterInjectedController.java
@@ -2,17 +2,19 @@ package org.daniels.spring.didemo.controllers;
 
 import org.daniels.spring.didemo.services.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class SetterInjectedController {
-    private GreetingService greetingService;
-
     public String sayHello(){
         return greetingService.sayGreeting();
     }
 
+    private GreetingService greetingService;
+
     @Autowired
+    @Qualifier("getterGreetingService")
     public void setGreetingService(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
---
+++ di-demo/src/main/java/org/daniels/spring/didemo/services/ConstructorGreetingService.java
@@ -0,0 +1,11 @@
+package org.daniels.spring.didemo.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ConstructorGreetingService implements GreetingService {
+    @Override
+    public String sayGreeting() {
+        return "Hello - I was injected via the constructor";
+    }
+}
