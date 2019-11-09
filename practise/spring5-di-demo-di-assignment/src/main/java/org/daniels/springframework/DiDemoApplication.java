81880cdd93df8e495e4e03d90944322f408975eb
daniels75 <daniel.sadowski75@gmail.com>
2019-11-09 20:21:27 +0100

    Added practise spring directory with first example

---
+++ practise/spring5-di-demo-di-assignment/src/main/java/org/daniels/springframework/DiDemoApplication.java
@@ -0,0 +1,24 @@
+package org.daniels.springframework;
+
+import org.daniels.springframework.controllers.ConstructorInjectedController;
+import org.daniels.springframework.controllers.GetterInjectedController;
+import org.daniels.springframework.controllers.MyController;
+import org.daniels.springframework.controllers.PropertyInjectedController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class DiDemoApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
+
+		MyController controller = (MyController) ctx.getBean("myController");
+
+		System.out.println(controller.hello());
+		System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
+		System.out.println(ctx.getBean(GetterInjectedController.class).sayHello());
+		System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
+	}
+}
