fee24f1b5ffbb2e1fb2bf00bb26f1125ba328da0
daniels75 <daniel.sadowski75@gmail.com>
2019-11-12 00:19:15 +0100

    Added Pet and PetType pojo's

---
+++ ds-pet-clinic/src/main/java/org/daniels/spring/dspetclinic/Pet.java
@@ -0,0 +1,35 @@
+package org.daniels.spring.dspetclinic;
+
+import org.daniels.spring.dspetclinic.model.Owner;
+
+import java.time.LocalDate;
+
+public class Pet {
+    private PetType petType;
+    private Owner owner;
+    private LocalDate birthData;
+
+    public PetType getPetType() {
+        return petType;
+    }
+
+    public void setPetType(PetType petType) {
+        this.petType = petType;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public LocalDate getBirthData() {
+        return birthData;
+    }
+
+    public void setBirthData(LocalDate birthData) {
+        this.birthData = birthData;
+    }
+}
---
+++ ds-pet-clinic/src/main/java/org/daniels/spring/dspetclinic/PetType.java
@@ -0,0 +1,13 @@
+package org.daniels.spring.dspetclinic;
+
+public class PetType {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
