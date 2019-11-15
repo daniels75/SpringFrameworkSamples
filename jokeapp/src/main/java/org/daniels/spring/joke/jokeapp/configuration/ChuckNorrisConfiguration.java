6d9df8d94190cab628b90e2e25f3dc9f3c827d34
daniels75 <daniel.sadowski75@gmail.com>
2019-11-15 21:05:20 +0100

    Added @Configuraiton and @Bean example

---
+++ jokeapp/src/main/java/org/daniels/spring/joke/jokeapp/configuration/ChuckNorrisConfiguration.java
@@ -0,0 +1,14 @@
+package org.daniels.spring.joke.jokeapp.configuration;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ChuckNorrisConfiguration {
+
+    @Bean
+    public ChuckNorrisQuotes createChuckNorrisQuotes(){
+        return new ChuckNorrisQuotes();
+    }
+}
