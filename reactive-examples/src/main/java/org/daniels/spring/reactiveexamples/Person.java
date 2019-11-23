570e4dc0b6ee5fdb489198e49fc6180e7ca26af5
daniels75 <daniel.sadowski75@gmail.com>
2019-11-23 20:23:40 +0100

    Added Spring reactive example

---
+++ reactive-examples/src/main/java/org/daniels/spring/reactiveexamples/Person.java
@@ -0,0 +1,22 @@
+package org.daniels.spring.reactiveexamples;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by jt on 8/24/17.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person {
+
+    private String firstName;
+    private String lastName;
+
+    public String sayMyName(){
+        return "My Name is " + firstName + " " + lastName + ".";
+    }
+
+}
---
+++ reactive-examples/src/main/java/org/daniels/spring/reactiveexamples/ReactiveExamplesApplication.java
@@ -0,0 +1,12 @@
+package org.daniels.spring.reactiveexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveExamplesApplication.class, args);
+	}
+}
