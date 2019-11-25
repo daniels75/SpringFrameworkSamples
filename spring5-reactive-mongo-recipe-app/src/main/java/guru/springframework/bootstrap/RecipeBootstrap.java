fb58750ff7227b54ece121cfaf7d69587e1cd5f5
daniels75 <daniel.sadowski75@gmail.com>
2019-11-25 22:25:59 +0100

    Updated services and controllers

--- spring5-reactive-mongo-recipe-app/src/main/java/guru/springframework/bootstrap/RecipeBootstrap.java
+++ spring5-reactive-mongo-recipe-app/src/main/java/guru/springframework/bootstrap/RecipeBootstrap.java
@@ -4,11 +4,7 @@ import guru.springframework.domain.*;
 import guru.springframework.repositories.CategoryRepository;
 import guru.springframework.repositories.RecipeRepository;
 import guru.springframework.repositories.UnitOfMeasureRepository;
-import guru.springframework.repositories.reactive.CategoryReactiveRepository;
-import guru.springframework.repositories.reactive.RecipeReactiveRepository;
-import guru.springframework.repositories.reactive.UnitOfMeasureReactiveRepository;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -30,15 +26,6 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
     private final RecipeRepository recipeRepository;
     private final UnitOfMeasureRepository unitOfMeasureRepository;
 
-    @Autowired
-    UnitOfMeasureReactiveRepository unitOfMeasureReactiveRepository;
-
-    @Autowired
-    CategoryReactiveRepository categoryReactiveRepository;
-
-    @Autowired
-    RecipeReactiveRepository recipeReactiveRepository;
-
     public RecipeBootstrap(CategoryRepository categoryRepository,
                            RecipeRepository recipeRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
         this.categoryRepository = categoryRepository;
@@ -50,18 +37,11 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
     @Transactional
     public void onApplicationEvent(ContextRefreshedEvent event) {
 
-        log.error("#######");
-        log.error("count before: " + unitOfMeasureReactiveRepository.count().block().toString());
-
         loadCategories();
         loadUom();
         recipeRepository.saveAll(getRecipes());
         log.debug("Loading Bootstrap Data");
 
-        log.error("#######");
-        log.error("count after load: " + unitOfMeasureReactiveRepository.count().block().toString());
-        log.error("count after load: " + recipeReactiveRepository.count().block().toString());
-        log.error("count after load: " + categoryReactiveRepository.count().block().toString());
     }
 
     private void loadCategories(){
