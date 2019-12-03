93d2dd60efea3ce22770011b1a39d1d69c1219a3
daniels75 <daniel.sadowski75@gmail.com>
2019-12-03 22:20:12 +0100

    Added a JMS Sender

--- spring5-jms/src/main/java/org/daniels/spring/jms/sender/HelloSender.java
+++ spring5-jms/src/main/java/org/daniels/spring/jms/sender/HelloSender.java
@@ -1,14 +1,33 @@
 package org.daniels.spring.jms.sender;
 
+import lombok.RequiredArgsConstructor;
+import org.daniels.spring.jms.config.JmsConfig;
+import org.daniels.spring.jms.model.HelloWorldMessage;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.util.UUID;
+
+@RequiredArgsConstructor
 @Component
 public class HelloSender {
+
+    private final JmsTemplate jmsTemplate;
 
     @Scheduled(fixedRate = 2000)
     public void sendMessage() {
         System.out.println("I'm sending a message");
+
+        HelloWorldMessage message = HelloWorldMessage
+                .builder()
+                .id(UUID.randomUUID())
+                .message("Hello World!")
+                .build();
+
+        jmsTemplate.convertAndSend(JmsConfig.MY_QUEUE, message);
+
+        System.out.println("Message sent");
     }
 
 }
