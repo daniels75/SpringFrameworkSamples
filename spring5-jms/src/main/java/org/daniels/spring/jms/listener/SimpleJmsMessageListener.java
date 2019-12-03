6256197acc373ad7667ace1891d62b605b32ebb1
daniels75 <daniel.sadowski75@gmail.com>
2019-12-03 23:02:04 +0100

    Added JmsListener example

---
+++ spring5-jms/src/main/java/org/daniels/spring/jms/listener/SimpleJmsMessageListener.java
@@ -0,0 +1,26 @@
+package org.daniels.spring.jms.listener;
+
+import org.daniels.spring.jms.config.JmsConfig;
+import org.daniels.spring.jms.model.HelloWorldMessage;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+import javax.jms.Message;
+
+@Component
+public class SimpleJmsMessageListener {
+
+    @JmsListener(destination = JmsConfig.MY_QUEUE)
+    public void listen(@Payload HelloWorldMessage helloWorldMessage,
+                       @Headers MessageHeaders headers,
+                       Message message) {
+        System.out.println("I got a message");
+        System.out.println(helloWorldMessage);
+
+    }
+
+
+}
