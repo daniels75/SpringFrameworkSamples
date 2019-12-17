6f6662135b721bee0ede800b6af4765d01c826bc
daniels75 <daniel.sadowski75@gmail.com>
2019-12-17 22:26:54 +0100

    Added logic of the todo-list in the app component

--- todo/frontend/src/app/app.component.ts
+++ todo/frontend/src/app/app.component.ts
@@ -14,19 +14,19 @@ export class AppComponent {
   constructor(private todoDataService: TodoDataService) {
   }
 
-  toggleTodoComplete(todo) {
-    this.todoDataService.toggleTodoComplete(todo);
-  }
-
-  removeTodo(todo) {
-    this.todoDataService.deleteTodoById(todo.id);
-  }
-
   get todos() {
     return this.todoDataService.getAllTodos();
   }
 
   onAddTodo(todo: Todo) {
     this.todoDataService.addTodo(todo);
+  }
+
+  onToggleTodoComplete(todo: Todo) {
+    this.todoDataService.toggleTodoComplete(todo);
+  }
+
+  onRemoveTodo(todo: Todo) {
+    this.todoDataService.deleteTodoById(todo.id);
   }
 }
