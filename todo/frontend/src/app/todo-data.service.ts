dc405cfe07f72dafd867d6eb76613c867284cc6b
daniels75 <daniel.sadowski75@gmail.com>
2019-12-20 19:48:43 +0100

    updated comments

--- todo/frontend/src/app/todo-data.service.ts
+++ todo/frontend/src/app/todo-data.service.ts
@@ -12,27 +12,27 @@ export class TodoDataService {
   constructor( private api: ApiService ) {
   }
 
-  // Simulate POST /todos
+  // POST /todos
   addTodo(todo: Todo): Observable<Todo> {
     return this.api.createTodo(todo);
   }
 
-  // Simulate DELETE /todos/:id
+  // DELETE /todos/:id
   deleteTodoById(todoId: number): Observable<Todo> {
     return this.api.deleteTodoById(todoId);
   }
 
-  // Simulate PUT /todos/:id
+  // PUT /todos/:id
   updateTodo(todo: Todo): Observable<Todo> {
     return this.api.updateTodo(todo);
   }
 
-  // Simulate GET /todos
+  // GET /todos
   getAllTodos(): Observable<Todo[]> {
     return this.api.getAllTodos();
   }
 
-  // Simulate GET /todos/:id
+  // GET /todos/:id
   getTodoById(todoId: number): Observable<Todo> {
     return this.api.getTodoById(todoId);
   }
