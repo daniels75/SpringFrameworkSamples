8c146334098fb8ceecf9a4f2672b6442f2cd5354
daniels75 <daniel.sadowski75@gmail.com>
2019-12-24 00:47:49 +0100

    Refactor of the ui - part 4 - item css

--- todo/frontend/src/app/todo-list-item/todo-list-item.component.ts
+++ todo/frontend/src/app/todo-list-item/todo-list-item.component.ts
@@ -14,6 +14,7 @@ export class TodoListItemComponent implements OnInit {
   @Output() remove: EventEmitter<Todo> = new EventEmitter();
 
   @Output() toggleComplete: EventEmitter<Todo> = new EventEmitter();
+  hover: boolean;
 
   constructor() {
   }
