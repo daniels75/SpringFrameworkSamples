c74e9d09b894e98b8e3772b5dc7a83198d449b97
daniels75 <daniel.sadowski75@gmail.com>
2020-01-02 19:49:10 +0100

    Adding error handling on the ui - part1

--- todo/frontend/src/app/api.service.ts
+++ todo/frontend/src/app/api.service.ts
@@ -128,10 +128,10 @@ export class ApiService {
     return (error: any): Observable<T> => {
 
       // TODO: send the error to remote logging infrastructure
-      console.error(error); // log to console instead
+      console.error("Error in the response: " + error); // log to console instead
 
       // Let the app keep running by returning an empty result.
-      return of(result as T);
+      return Observable.throw(error);
     }
   };
 
--- todo/frontend/src/app/app.component.ts
+++ todo/frontend/src/app/app.component.ts
@@ -41,6 +41,9 @@ export class AppComponent implements OnInit {
       .subscribe(
         (newTodo) => {
           this.todos = this.todos.concat(newTodo);
+        },
+        (error) => {
+          console.log("error");
         }
       );
   }
