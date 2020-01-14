b3133bdafc65d4f72ad5dc9892956314af553ac0
daniels75 <daniel.sadowski75@gmail.com>
2020-01-14 20:42:23 +0100

    Added Spring cache example - part1

---
+++ cache-example2/src/main/java/org/daniels/spring/cache/cacheexample2/CacheExample2Application.java
@@ -0,0 +1,13 @@
+package org.daniels.spring.cache.cacheexample2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacheExample2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CacheExample2Application.class, args);
+	}
+
+}
---
+++ cache-example2/src/test/java/org/daniels/spring/cache/cacheexample2/CacheExample2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.daniels.spring.cache.cacheexample2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CacheExample2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
