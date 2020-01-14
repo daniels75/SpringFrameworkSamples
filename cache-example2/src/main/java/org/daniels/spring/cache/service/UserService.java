d7859a56852f724e321735f465e9008c261e57a3
daniels75 <daniel.sadowski75@gmail.com>
2020-01-14 21:35:04 +0100

    Added service layer

---
+++ cache-example2/src/main/java/org/daniels/spring/cache/service/UserService.java
@@ -0,0 +1,54 @@
+package org.daniels.spring.cache.service;
+
+import org.daniels.spring.cache.domain.User;
+import org.daniels.spring.cache.mapper.UserMapper;
+import org.daniels.spring.cache.model.UserDTO;
+import org.daniels.spring.cache.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    public UserService(UserRepository userRepository, UserMapper userMapper) {
+        this.userRepository = userRepository;
+        this.userMapper = userMapper;
+    }
+
+    public UserDTO add(UserDTO userDTO) {
+        User createdEntity = userRepository.save(userMapper.toUser(userDTO));
+
+        return userMapper.toDTO(createdEntity);
+    }
+
+    public List<UserDTO> retrieveAll() {
+        List<UserDTO> todoList = userRepository.findAll()
+                .stream()
+                .map(userMapper::toDTO)
+                .collect(Collectors.toList());
+        return todoList;
+    }
+
+    public UserDTO update(UserDTO userDTO) {
+        User updateEntity = userRepository.save(userMapper.toUser(userDTO));
+
+        return userMapper.toDTO(updateEntity);
+    }
+
+    public UserDTO findById(Long id) {
+        Optional<User> todo = userRepository.findById(id);
+
+        return userMapper.toDTO(todo.get());
+    }
+
+    public void delete(Long id) {
+        userRepository.deleteById(id);
+    }
+
+}
