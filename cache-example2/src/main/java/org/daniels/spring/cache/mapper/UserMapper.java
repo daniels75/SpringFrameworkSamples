d2e749f5cf6b812f5c65748b1f26fe9cc4eec24b
daniels75 <daniel.sadowski75@gmail.com>
2020-01-14 21:11:07 +0100

    Added DTO and mapper

---
+++ cache-example2/src/main/java/org/daniels/spring/cache/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package org.daniels.spring.cache.mapper;
+
+import org.daniels.spring.cache.domain.User;
+import org.daniels.spring.cache.model.UserDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface UserMapper {
+    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+
+    UserDTO toDTO(User user);
+
+    User toUser(UserDTO userDTO);
+}
---
+++ cache-example2/src/main/java/org/daniels/spring/cache/model/UserDTO.java
@@ -0,0 +1,59 @@
+package org.daniels.spring.cache.model;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
+
+public class UserDTO {
+    private Long id;
+    @Size(max = 50)
+    private String firstName;
+
+    @Size(max = 50)
+    private String lastName;
+
+    @Email
+    @Size(min = 5, max = 254)
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "UserDTO {" +
+                " id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
