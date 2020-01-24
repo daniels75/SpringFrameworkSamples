8303cf508a564e329164c61a015448f4910dcb6a
daniels75 <daniel.sadowski75@gmail.com>
2020-01-24 20:23:23 +0100

    Added tutorial

---
+++ tutorial/spring-bean-annotations/src/main/java/org/daniels/spring/tutorial/SpringBeanAnnotationsApplication.java
@@ -0,0 +1,13 @@
+package org.daniels.spring.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBeanAnnotationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBeanAnnotationsApplication.class, args);
+	}
+
+}
---
+++ tutorial/spring-bean-annotations/src/test/java/org/daniels/spring/tutorial/SpringBeanAnnotationsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.daniels.spring.tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBeanAnnotationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
