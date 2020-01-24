dd9fd8ca7ff12d91eb1232f97e53e16e5d63386c
daniels75 <daniel.sadowski75@gmail.com>
2020-01-24 20:32:52 +0100

    Added logger

--- tutorial/spring-bean-annotations/src/main/java/org/daniels/spring/tutorial/SpringBeanAnnotationsApplication.java
+++ tutorial/spring-bean-annotations/src/main/java/org/daniels/spring/tutorial/SpringBeanAnnotationsApplication.java
@@ -1,13 +1,19 @@
 package org.daniels.spring.tutorial;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBeanAnnotationsApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(SpringBeanAnnotationsApplication.class);
+
 	public static void main(String[] args) {
+		log.info("Spring bean annotations example");
 		SpringApplication.run(SpringBeanAnnotationsApplication.class, args);
+
 	}
 
 }
