1133e86ae552aee59bd89f67af815a6738076cab
daniels75 <daniel.sadowski75@gmail.com>
2020-01-27 22:14:36 +0100

    Added spring jwt demo - part

---
+++ tutorial/spring-jwt-demo/src/main/java/org/daniels/spring/tutorial/jwtdemo/SpringjwtDemoApplication.java
@@ -0,0 +1,13 @@
+package org.daniels.spring.tutorial.jwtdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringjwtDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringjwtDemoApplication.class, args);
+	}
+
+}
---
+++ tutorial/spring-jwt-demo/src/test/java/org/daniels/spring/tutorial/jwtdemo/SpringjwtDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.daniels.spring.tutorial.jwtdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringjwtDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
