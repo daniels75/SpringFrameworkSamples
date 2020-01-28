18dda5b5e14cee8c76e1061857046c23de634ece
daniels75 <daniel.sadowski75@gmail.com>
2020-01-28 22:38:22 +0100

    Addes simple model

---
+++ tutorial/spring-jwt-demo/src/main/java/org/daniels/spring/tutorial/jwtdemo/model/Employee.java
@@ -0,0 +1,33 @@
+package org.daniels.spring.tutorial.jwtdemo.model;
+
+public class Employee {
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Employee id(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Employee name(String name) {
+        this.name = name;
+        return this;
+    }
+
+}
