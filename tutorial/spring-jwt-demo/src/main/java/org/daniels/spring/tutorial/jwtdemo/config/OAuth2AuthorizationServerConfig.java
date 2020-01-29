27fef62c68ce4061bd87254a9be2ffe20bbf1af4
daniels75 <daniel.sadowski75@gmail.com>
2020-01-29 20:08:36 +0100

    Added oauth dependency

---
+++ tutorial/spring-jwt-demo/src/main/java/org/daniels/spring/tutorial/jwtdemo/config/OAuth2AuthorizationServerConfig.java
@@ -0,0 +1,11 @@
+package org.daniels.spring.tutorial.jwtdemo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+@Configuration
+@EnableAuthorizationServer
+public class OAuth2AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+    
+}
