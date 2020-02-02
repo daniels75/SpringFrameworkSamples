99943a6b234fc36cea2a8258d5b5d5314e63cf68
daniels75 <daniel.sadowski75@gmail.com>
2020-02-02 21:08:19 +0100

    Added sso server project

---
+++ spring-security-sso-auth-server/src/main/java/org/daniels/spring/sso/SpringSecuritySsoAuthServerApplication.java
@@ -0,0 +1,13 @@
+package org.daniels.spring.sso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecuritySsoAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecuritySsoAuthServerApplication.class, args);
+	}
+
+}
---
+++ spring-security-sso-auth-server/src/test/java/org/daniels/spring/sso/SpringSecuritySsoAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.daniels.spring.sso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecuritySsoAuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
