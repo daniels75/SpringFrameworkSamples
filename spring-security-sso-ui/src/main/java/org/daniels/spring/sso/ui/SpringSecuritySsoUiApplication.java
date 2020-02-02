517884912be1b96474c20769773a464c8a4a5139
daniels75 <daniel.sadowski75@gmail.com>
2020-02-02 21:26:15 +0100

    Spring security sso ui

---
+++ spring-security-sso-ui/src/main/java/org/daniels/spring/sso/ui/SpringSecuritySsoUiApplication.java
@@ -0,0 +1,13 @@
+package org.daniels.spring.sso.ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecuritySsoUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecuritySsoUiApplication.class, args);
+	}
+
+}
---
+++ spring-security-sso-ui/src/test/java/org/daniels/spring/sso/ui/SpringSecuritySsoUiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.daniels.spring.sso.ui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecuritySsoUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
