9e0808e508e1c7cc2091f768e02b558a4e876db0
daniels75 <daniel.sadowski75@gmail.com>
2020-02-02 21:51:41 +0100

    Added configuration for ui

--- spring-security-sso-ui/src/main/java/org/daniels/spring/sso/ui/SpringSecuritySsoUiApplication.java
+++ spring-security-sso-ui/src/main/java/org/daniels/spring/sso/ui/SpringSecuritySsoUiApplication.java
@@ -2,6 +2,8 @@ package org.daniels.spring.sso.ui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.context.request.RequestContextListener;
 
 @SpringBootApplication
 public class SpringSecuritySsoUiApplication {
@@ -10,4 +12,8 @@ public class SpringSecuritySsoUiApplication {
 		SpringApplication.run(SpringSecuritySsoUiApplication.class, args);
 	}
 
+	@Bean
+	public RequestContextListener requestContextListener() {
+		return new RequestContextListener();
+	}
 }
