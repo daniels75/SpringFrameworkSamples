c9d55d2409b7676b7fc8949972f9bb62724cca74
daniels75 <daniel.sadowski75@gmail.com>
2020-02-02 23:31:51 +0100

    Added spring sso server

---
+++ spring-sso-server/src/main/java/org/daniels/spring/sso/server/SpringSsoServerApplication.java
@@ -0,0 +1,13 @@
+package org.daniels.spring.sso.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSsoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSsoServerApplication.class, args);
+	}
+
+}
---
+++ spring-sso-server/src/test/java/org/daniels/spring/sso/server/SpringSsoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.daniels.spring.sso.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSsoServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
