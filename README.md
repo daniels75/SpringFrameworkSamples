4e8494f47dad2681c3f3b1f21290a48ad0df86e4
daniels75 <daniel.sadowski75@gmail.com>
2020-02-03 00:05:22 +0100

    Added config for first ui app

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 ## sso example
 - spring-security-sso-auth-server
 - spring-security-sso-ui
+
+## another sso example
+- spring-sso-server
+- spring-sso-ui-first
--- spring-sso-ui-first/src/main/java/org/daniels/spring/sso/ui/first/SpringSsoUiFirstApplication.java
+++ spring-sso-ui-first/src/main/java/org/daniels/spring/sso/ui/first/SpringSsoUiFirstApplication.java
@@ -2,12 +2,19 @@ package org.daniels.spring.sso.ui.first;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.context.request.RequestContextListener;
 
 @SpringBootApplication
 public class SpringSsoUiFirstApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSsoUiFirstApplication.class, args);
+	}
+
+	@Bean
+	public RequestContextListener requestContextListener() {
+		return new RequestContextListener();
 	}
 
 }
