e5b41d21f6e0af2ff98881ee6795c69fc4563521
daniels75 <daniel.sadowski75@gmail.com>
2020-02-02 21:17:23 +0100

    Added configuration for auth server

--- spring-security-sso-auth-server/src/main/java/org/daniels/spring/sso/SpringSecuritySsoAuthServerApplication.java
+++ spring-security-sso-auth-server/src/main/java/org/daniels/spring/sso/SpringSecuritySsoAuthServerApplication.java
@@ -2,9 +2,12 @@ package org.daniels.spring.sso;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
-public class SpringSecuritySsoAuthServerApplication {
+@EnableResourceServer
+public class SpringSecuritySsoAuthServerApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecuritySsoAuthServerApplication.class, args);
