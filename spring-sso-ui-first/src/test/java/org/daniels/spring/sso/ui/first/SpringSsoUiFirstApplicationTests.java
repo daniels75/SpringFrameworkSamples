7c1e9d748d64d8db0cdde038ea43f9e40864ab47
daniels75 <daniel.sadowski75@gmail.com>
2020-02-02 23:48:12 +0100

    Added first ui for sso

---
+++ spring-sso-ui-first/src/test/java/org/daniels/spring/sso/ui/first/SpringSsoUiFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package org.daniels.spring.sso.ui.first;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSsoUiFirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
