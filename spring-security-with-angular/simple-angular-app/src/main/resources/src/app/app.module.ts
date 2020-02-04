ad93cd931a060946f807b931ffafa0f7db7cae40
daniels75 <daniel.sadowski75@gmail.com>
2020-02-04 23:21:05 +0100

    Move routing to the AppRoutingModule

--- spring-security-with-angular/simple-angular-app/src/main/resources/src/app/app.module.ts
+++ spring-security-with-angular/simple-angular-app/src/main/resources/src/app/app.module.ts
@@ -3,13 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {HttpClientModule} from "@angular/common/http";
-import {RouterModule, Routes} from "@angular/router";
 import {HomeComponent} from "./home.component";
 import {FooComponent} from "./foo.component";
-
-const routes: Routes = [
-  { path: '', component: HomeComponent, pathMatch: 'full' }
-];
+import {AppRoutingModule} from "./app-routing.module";
 
 
 @NgModule({
@@ -21,7 +17,7 @@ const routes: Routes = [
   imports: [
     BrowserModule,
     HttpClientModule,
-    RouterModule.forRoot(routes, {onSameUrlNavigation: 'reload'})
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
