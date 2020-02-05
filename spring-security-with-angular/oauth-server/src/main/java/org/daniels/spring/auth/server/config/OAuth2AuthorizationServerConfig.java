1fc94aec8bc08d4f8c67ddfe42ea1740221e6650
daniels75 <daniel.sadowski75@gmail.com>
2020-02-05 21:05:04 +0100

    Added configure with permitAll

--- spring-security-with-angular/oauth-server/src/main/java/org/daniels/spring/auth/server/config/OAuth2AuthorizationServerConfig.java
+++ spring-security-with-angular/oauth-server/src/main/java/org/daniels/spring/auth/server/config/OAuth2AuthorizationServerConfig.java
@@ -3,9 +3,16 @@ package org.daniels.spring.auth.server.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 
 @Configuration
 @EnableAuthorizationServer
 public class OAuth2AuthorizationServerConfig  extends AuthorizationServerConfigurerAdapter {
 
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        security
+                .tokenKeyAccess("permitAll()")
+                .checkTokenAccess("isAuthenticated()");
+    }
 }
