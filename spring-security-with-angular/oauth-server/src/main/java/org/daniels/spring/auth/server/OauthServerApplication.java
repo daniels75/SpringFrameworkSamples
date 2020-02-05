0b3a47405688f190025d0eeb2ddab24c4402f7b2
daniels75 <daniel.sadowski75@gmail.com>
2020-02-05 19:22:13 +0100

    Added oauth server - part1

---
+++ spring-security-with-angular/oauth-server/src/main/java/org/daniels/spring/auth/server/OauthServerApplication.java
@@ -0,0 +1,13 @@
+package org.daniels.spring.auth.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthServerApplication.class, args);
+	}
+
+}
