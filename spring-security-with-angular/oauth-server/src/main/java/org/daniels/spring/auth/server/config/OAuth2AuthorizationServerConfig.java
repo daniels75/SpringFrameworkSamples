8ae44ce466d925d15686b15576031b3a9a38e945
daniels75 <daniel.sadowski75@gmail.com>
2020-02-05 22:21:01 +0100

    Spring OAuth + client - part 1

--- spring-security-with-angular/oauth-server/src/main/java/org/daniels/spring/auth/server/config/OAuth2AuthorizationServerConfig.java
+++ spring-security-with-angular/oauth-server/src/main/java/org/daniels/spring/auth/server/config/OAuth2AuthorizationServerConfig.java
@@ -1,6 +1,10 @@
 package org.daniels.spring.auth.server.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
@@ -13,5 +17,21 @@ public class OAuth2AuthorizationServerConfig  extends AuthorizationServerConfigu
         security
                 .tokenKeyAccess("permitAll()")
                 .checkTokenAccess("isAuthenticated()");
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients
+                .inMemory()
+                .withClient("fooClientIdPassword")
+                .secret(passwordEncoder().encode("secret"))
+                .scopes("user_info")
+                .authorizedGrantTypes("password", "authorization_code", "refresh_token", "client_credentials")
+                .redirectUris("http://localhost:8086/simple-ui/login/oauth2/code/custom");
     }
 }
