fefe985e53aa35d1fda5be483e3a6a4fa980e25e
daniels75 <daniel.sadowski75@gmail.com>
2020-02-05 23:43:57 +0100

    Added config method to the Oauth server config

--- spring-security-with-angular/oauth-server/src/main/java/org/daniels/spring/auth/server/OauthServerApplication.java
+++ spring-security-with-angular/oauth-server/src/main/java/org/daniels/spring/auth/server/OauthServerApplication.java
@@ -2,7 +2,6 @@ package org.daniels.spring.auth.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
