056906e9bba8454ca1375cc234df98fcada1b208
daniels75 <daniel.sadowski75@gmail.com>
2020-02-11 19:45:46 +0100

    Added user simple REST

--- spring-security-with-angular/oauth-server/src/main/java/org/daniels/spring/auth/server/config/UserController.java
+++ spring-security-with-angular/oauth-server/src/main/java/org/daniels/spring/auth/server/config/UserController.java
@@ -1,6 +1,7 @@
 package org.daniels.spring.auth.server.config;
 
 
+import org.daniels.spring.auth.server.model.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,9 @@ public class UserController {
         return principal;
     }
 
-
+    @RequestMapping("/simple")
+    public User findUser() {
+        return new User().id(1L).name("john").surname("kowalski");
+    }
 
 }
