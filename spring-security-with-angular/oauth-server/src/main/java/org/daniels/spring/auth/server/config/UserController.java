36bcebd14182a36839a2a60b16b9b305b6ae0de3
daniels75 <daniel.sadowski75@gmail.com>
2020-02-11 19:37:40 +0100

    Change Rest mapping

--- spring-security-with-angular/oauth-server/src/main/java/org/daniels/spring/auth/server/config/UserController.java
+++ spring-security-with-angular/oauth-server/src/main/java/org/daniels/spring/auth/server/config/UserController.java
@@ -2,16 +2,21 @@ package org.daniels.spring.auth.server.config;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
 
 @RestController
+@RequestMapping("/user")
 public class UserController {
 
-    @GetMapping("/user/me")
+    @GetMapping("/me")
     public Principal user(Principal principal) {
         return principal;
     }
+
+
 
 }
