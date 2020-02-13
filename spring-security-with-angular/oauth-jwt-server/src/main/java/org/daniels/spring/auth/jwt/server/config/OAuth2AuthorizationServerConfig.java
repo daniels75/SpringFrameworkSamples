a1852efa951f7c078b3657f4196599bd7c0c27b7
daniels75 <daniel.sadowski75@gmail.com>
2020-02-13 23:31:49 +0100

    Added config for jwt

--- spring-security-with-angular/oauth-jwt-server/src/main/java/org/daniels/spring/auth/jwt/server/config/OAuth2AuthorizationServerConfig.java
+++ spring-security-with-angular/oauth-jwt-server/src/main/java/org/daniels/spring/auth/jwt/server/config/OAuth2AuthorizationServerConfig.java
@@ -1,12 +1,17 @@
 package org.daniels.spring.auth.jwt.server.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 @EnableAuthorizationServer
@@ -43,4 +48,25 @@ public class OAuth2AuthorizationServerConfig  extends AuthorizationServerConfigu
                         );
     }
 
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
+        // @formatter:off
+        endpoints
+                .authenticationManager(this.authenticationManager)
+                .accessTokenConverter(accessTokenConverter())
+                .tokenStore(tokenStore());
+        // @formatter:on
+    }
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(accessTokenConverter());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        converter.setSigningKey("2020");
+        return converter;
+    }
 }
