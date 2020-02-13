e42a3d9be084325ad793c88da1bc9dc919e86155
daniels75 <daniel.sadowski75@gmail.com>
2020-02-13 19:25:35 +0100

    Added AuthenticationManager to the config

--- spring-security-with-angular/oauth-server/src/main/java/org/daniels/spring/auth/server/config/OAuth2AuthorizationServerConfig.java
+++ spring-security-with-angular/oauth-server/src/main/java/org/daniels/spring/auth/server/config/OAuth2AuthorizationServerConfig.java
@@ -39,4 +39,5 @@ public class OAuth2AuthorizationServerConfig  extends AuthorizationServerConfigu
                         );
     }
 
+
 }
--- spring-security-with-angular/oauth-server/src/main/java/org/daniels/spring/auth/server/config/WebSecurityConfig.java
+++ spring-security-with-angular/oauth-server/src/main/java/org/daniels/spring/auth/server/config/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package org.daniels.spring.auth.server.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -34,6 +35,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 
 }
