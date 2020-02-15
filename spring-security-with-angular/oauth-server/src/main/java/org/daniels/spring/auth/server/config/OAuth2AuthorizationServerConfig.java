42fcc79e8d4b9ba8301638f70554b44c35604ca9
daniels75 <daniel.sadowski75@gmail.com>
2020-02-15 19:37:46 +0100

    Cleanup in the code

--- spring-security-with-angular/oauth-server/src/main/java/org/daniels/spring/auth/server/config/OAuth2AuthorizationServerConfig.java
+++ spring-security-with-angular/oauth-server/src/main/java/org/daniels/spring/auth/server/config/OAuth2AuthorizationServerConfig.java
@@ -11,13 +11,8 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
-import org.springframework.security.oauth2.provider.token.TokenEnhancer;
-import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
-import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
-
-import java.util.Arrays;
 
 @Configuration
 @EnableAuthorizationServer
