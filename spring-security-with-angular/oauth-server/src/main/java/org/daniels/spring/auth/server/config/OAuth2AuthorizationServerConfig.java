0f944d1894f03fc2f8af4f8deb310b97fa5178df
daniels75 <daniel.sadowski75@gmail.com>
2020-02-15 19:30:34 +0100

    Changed configuraiton of auth server added token store

--- spring-security-with-angular/oauth-server/src/main/java/org/daniels/spring/auth/server/config/OAuth2AuthorizationServerConfig.java
+++ spring-security-with-angular/oauth-server/src/main/java/org/daniels/spring/auth/server/config/OAuth2AuthorizationServerConfig.java
@@ -1,20 +1,34 @@
 package org.daniels.spring.auth.server.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableAuthorizationServer
 public class OAuth2AuthorizationServerConfig  extends AuthorizationServerConfigurerAdapter {
 
     private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
 
-    public OAuth2AuthorizationServerConfig(PasswordEncoder passwordEncoder) {
+    public OAuth2AuthorizationServerConfig(PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager) {
         this.passwordEncoder = passwordEncoder;
+        this.authenticationManager = authenticationManager;
     }
 
     @Override
@@ -39,5 +53,26 @@ public class OAuth2AuthorizationServerConfig  extends AuthorizationServerConfigu
                         );
     }
 
+    @Override
+    public void configure(final AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        // @formatter:off
+        endpoints.tokenStore(tokenStore())
+               .authenticationManager(authenticationManager);
+        // @formatter:on
+    }
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new InMemoryTokenStore();
+    }
+
+    @Bean
+    @Primary
+    public DefaultTokenServices tokenServices() {
+        final DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+        defaultTokenServices.setTokenStore(tokenStore());
+        defaultTokenServices.setSupportRefreshToken(true);
+        return defaultTokenServices;
+    }
 
 }
