c5d184634ee87517ff3c836b3860405c5327eabe
daniels75 <daniel.sadowski75@gmail.com>
2020-02-24 22:22:43 +0100

    Added REST for Foo controller

--- spring-security-with-angular/oauth-resource-server/src/main/java/org/daniels/spring/auth/resource/controller/FooController.java
+++ spring-security-with-angular/oauth-resource-server/src/main/java/org/daniels/spring/auth/resource/controller/FooController.java
@@ -1,10 +1,28 @@
 package org.daniels.spring.auth.resource.controller;
 
+import org.daniels.spring.auth.resource.model.Foo;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
+import static org.apache.commons.lang3.RandomStringUtils.randomNumeric;
 
 @Controller
 public class FooController {
 
+    @RequestMapping(method = RequestMethod.GET, value = "/foos/{id}")
+    @ResponseBody
+    public Foo findById(@PathVariable final long id) {
+        return new Foo(Long.parseLong(randomNumeric(2)), randomAlphabetic(4));
+    }
 
+    @RequestMapping(method = RequestMethod.POST, value = "/foos")
+    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseBody
+    public Foo create(@RequestBody final Foo foo) {
+        foo.setId(Long.parseLong(randomNumeric(2)));
+        return foo;
+    }
 
 }
