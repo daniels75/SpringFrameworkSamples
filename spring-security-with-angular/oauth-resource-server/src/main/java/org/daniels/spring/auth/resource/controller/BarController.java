7fca270195538251c5a2ef2daa012c74e7c0d6d2
daniels75 <daniel.sadowski75@gmail.com>
2020-02-25 18:16:03 +0100

    Added Bar controller

--- spring-security-with-angular/oauth-resource-server/src/main/java/org/daniels/spring/auth/resource/controller/BarController.java
+++ spring-security-with-angular/oauth-resource-server/src/main/java/org/daniels/spring/auth/resource/controller/BarController.java
@@ -1,10 +1,28 @@
 package org.daniels.spring.auth.resource.controller;
 
+import org.daniels.spring.auth.resource.model.Bar;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
+import static org.apache.commons.lang3.RandomStringUtils.randomNumeric;
 
 @Controller
 public class BarController {
 
+    @RequestMapping(method = RequestMethod.GET, value = "/bars/{id}")
+    @ResponseBody
+    public Bar findById(@PathVariable final long id) {
+        return new Bar(Long.parseLong(randomNumeric(2)), randomAlphabetic(4));
+    }
 
+    @RequestMapping(method = RequestMethod.POST, value = "/bars")
+    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseBody
+    public Bar create(@RequestBody final Bar bar) {
+        bar.setId(Long.parseLong(randomNumeric(2)));
+        return bar;
+    }
 
 }
