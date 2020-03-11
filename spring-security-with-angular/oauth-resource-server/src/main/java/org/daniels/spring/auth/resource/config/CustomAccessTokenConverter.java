28e3f21418002273d821da21d939ebd000423bf4
daniels75 <daniel.sadowski75@gmail.com>
2020-03-11 19:30:33 +0100

    Commented out some code

--- spring-security-with-angular/oauth-resource-server/src/main/java/org/daniels/spring/auth/resource/config/CustomAccessTokenConverter.java
+++ spring-security-with-angular/oauth-resource-server/src/main/java/org/daniels/spring/auth/resource/config/CustomAccessTokenConverter.java
@@ -12,7 +12,7 @@ public class CustomAccessTokenConverter extends DefaultAccessTokenConverter {
     @Override
     public OAuth2Authentication extractAuthentication(Map<String, ?> claims) {
         OAuth2Authentication authentication = super.extractAuthentication(claims);
-        authentication.setDetails(claims);
+        // TODO - DSA - authentication.setDetails(claims);
         return authentication;
     }
 }
