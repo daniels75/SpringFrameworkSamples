a108c6fef3fc519031b35d24c6463c2207191a47
daniels75 <daniel.sadowski75@gmail.com>
2021-03-04 21:45:50 +0100

    Added simple RestTemplate example

--- spring-rest-template/src/main/java/org/daniels/spring/resttemplate/SpringRestTemplateApplication.java
+++ spring-rest-template/src/main/java/org/daniels/spring/resttemplate/SpringRestTemplateApplication.java
@@ -2,6 +2,8 @@ package org.daniels.spring.resttemplate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class SpringRestTemplateApplication {
@@ -10,4 +12,8 @@ public class SpringRestTemplateApplication {
 		SpringApplication.run(SpringRestTemplateApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate createRestTemplate() {
+		return new RestTemplate();
+	}
 }
--- spring-rest-template/src/main/java/org/daniels/spring/resttemplate/rest/CarComplexController.java
+++ spring-rest-template/src/main/java/org/daniels/spring/resttemplate/rest/CarComplexController.java
@@ -2,9 +2,15 @@ package org.daniels.spring.resttemplate.rest;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import com.fasterxml.jackson.databind.util.JSONWrappedObject;
 import com.google.common.collect.Maps;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.daniels.spring.resttemplate.model.Car;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.RequestEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +19,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Map;
 
@@ -22,6 +29,11 @@ import java.util.Map;
 public class CarComplexController {
 
     private static final Map<String, Car> simpleMap = Maps.newHashMap();
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    private final String baseUrl = "http://localhost:8093";
 
     @PostMapping(path = "/{key}", produces= MediaType.APPLICATION_JSON_VALUE)
     public Car put(@PathVariable String key, @RequestBody Car car) {
@@ -36,11 +48,32 @@ public class CarComplexController {
         return simpleMap.get(key);
     }
 
+    @PostMapping(path = "/template/{key}", produces= MediaType.APPLICATION_JSON_VALUE)
+    public String putViaRestTemplate(@PathVariable String key, @RequestBody Car car) {
+        log.info("putViaRestTemplate key: {}, car: {}", key, car);
+        logJson(car);
+
+        String pathUrl = baseUrl  + "/complex/" + key;
+        log.info("putViaRestTemplate - put with path: {}", pathUrl);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<Car> request = new HttpEntity<Car>(car, headers);
+
+        Object result = restTemplate.postForEntity(pathUrl, car, Car.class);
+
+
+        //Object result = restTemplate.postForEntity(pathUrl, request, Car.class);
+
+
+        return "Added Car via REST Template";
+    }
+
     private void logJson(Object obj) {
         try {
             ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
             String json = ow.writeValueAsString(obj);
-            log.info("Json object: {}" + json);
+            log.info("Json object: {}", json);
         } catch (Exception e) {
             log.error("Cannot serialize object to json.", e);
         }
